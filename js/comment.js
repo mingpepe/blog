33db634c869abe88aef273ad93759331265f90b0
mingpepe <mingpepemath@gmail.com>
2022-05-31 13:38:28 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -5,7 +5,9 @@ function loadGithubComments(repo, issueid) {
       var content = data[i].body;
       $("#githubissue_comments").append("<div class=comments><p>name : " + name + "</p><p> content : " + content + "</p></div>");
     }
-    $("#githubissue_comments").append(`<a href='https://github.com/${repo}/blog/issues/${issueid}#new_comment_field}` class='button'>Leave comments</button>");
+    url = `https://github.com/${repo}/blog/issues/${issueid}#new_comment_field}`
+    console.log("Leave comments at " + url)
+    $("#githubissue_comments").append(`<a href='${url}'` class='button'>Leave comments</button>");
   }
   console.log(`Send get request to get comments according to issue id, repo = ${repo}, issueid = ${issueid}`)
   $.ajax(`https://api.github.com/repos/${repo}/blog/issues/${issueid}/comments`, {
