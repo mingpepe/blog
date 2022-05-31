4db626ec674f19d9cf0fbda8cd91f955ac431516
mingpepe <mingpepemath@gmail.com>
2022-05-31 13:06:49 +0800

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,7 +1,4 @@
 function loadGithubComments(repo, issueid) {
-}
-
-document.addEventListener("DOMContentLoaded", function(event) {
   function loadComments(data) {
     for (var i=0; i<data.length; i++) {
       var name = data[i].user.login;
@@ -11,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     $("#githubissue_comments").append("<a href='https://github.com/{{site.GITHUB_REPO}}/blog/issues/{{page.issueid}}#new_comment_field}' class='button'>Leave comments</button>");
   }
   console.log('Send get request to get comments according to issue id')
-  $.ajax("https://api.github.com/repos/{{site.GITHUB_REPO}}/blog/issues/{{page.issueid}}/comments", {
+  $.ajax("https://api.github.com/repos/${repo}/blog/issues/${issueid}/comments", {
     headers: {Accept: "application/vnd.github.squirrel-girl-preview.full+json"},
     dataType: "json",
     success: function(msg){
@@ -19,4 +16,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
       loadComments(msg);
     }
   });
-});
+}
